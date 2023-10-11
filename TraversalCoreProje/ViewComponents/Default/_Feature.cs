013a4ec5b3a52660250f555ffa8e0ad1f59edbc6
Ismail BILIR <ismailbiliren@gmail.com>
2023-10-11 12:16:05 +0300

    Eğitim = 17,  Dk = 12:50

---
+++ TraversalCoreProje/ViewComponents/Default/_Feature.cs
@@ -0,0 +1,14 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProje.ViewComponents.Default
+{
+    public class _Feature:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+           
+            return View();
+        }
+    }
+}
