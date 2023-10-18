1ffc17596d59cd3d8b5ab38102ee2ec696a4e017
Ismail BILIR <ismailbiliren@gmail.com>
2023-10-18 22:12:36 +0300

    Eğitim 29 sonuuu.

---
+++ TraversalCoreProje/Models/UserRegisterViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TraversalCoreProje.Models
+{
+	public class UserRegisterViewModel
+	{
+		[Required(ErrorMessage = "Lütfen adınızı girin!")]
+		public string Name { get; set; }
+
+		[Required(ErrorMessage = "Lütfen Soyadınızı girin!")]
+		public string Surname { get; set; }
+
+		[Required(ErrorMessage = "Lütfen kullanıcı adınızı girin!")]
+		public string Username { get; set; }
+
+		[Required(ErrorMessage = "Lütfen Mail adresinizi girin!")]
+		public string Mail { get; set; }
+
+		[Required(ErrorMessage = "Lütfen şifrenizi girin!")]
+		public string Password { get; set; }
+
+		[Required(ErrorMessage = "Lütfen adınızı girin!")]
+		[Compare("Password", ErrorMessage = "Şifreniz uyumlu değil!")]
+		public string ConfirmPassword { get; set; }
+
+	}
+}
