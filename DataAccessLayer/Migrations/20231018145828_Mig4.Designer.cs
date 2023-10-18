7dbabf90e82f915fe1d062897fb84184e6c3c4e9
Ismail BILIR <ismailbiliren@gmail.com>
2023-10-18 18:11:18 +0300

    Eğitim 27 sonu Identity kütüphanesi eklendi...

--- DataAccessLayer/Migrations/20231018145828_Mig4.Designer.cs
+++ DataAccessLayer/Migrations/20231018145828_Mig4.Designer.cs
@@ -4,14 +4,16 @@ using DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20231018145828_Mig4")]
+    partial class Mig4
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
